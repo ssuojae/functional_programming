2a71ef8b7bfc8711c1e99ae11add687babcf3efb
suojae <suojae.developer@gmail.com>
2024-09-09 01:32:23 +0900

    🎉 initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+function add(a, b) {
+    return a + b;
+}
+
+console.log(add(10, 5));
+console.log(add(10, 5));
+
+var c = 10
+function add2(a, b) {
+    return a + b + c;
+}
+
+console.log(add2(5, 10));
